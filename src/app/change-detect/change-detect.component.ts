ff576b45f9476cc40b667d35fda9b199309949d1
guodan1 <guodan1@lenovo.com>
2018-09-26 18:54:14 +0800

    change detection test 4
    Signed-off-by: guodan1 <guodan1@lenovo.com>

--- src/app/change-detect/change-detect.component.ts
+++ src/app/change-detect/change-detect.component.ts
@@ -12,25 +12,29 @@ export class ChangeDetectComponent implements OnInit {
     first: "guo",
     second: "dan"
   };
+  age = "27";
 
   get runChangeDetection(): boolean {
     console.log('Checking the parent view');
     return true;
   }
+
   constructor(private cdr: ChangeDetectorRef) { }
 
   ngOnInit() {
-    /**
-     * 这个定时器函数，可以证明，
-     * 当change detection strategy变成OnPush时，
-     * 只有Input的reference改变才会触发change detection
-     */
+
     setTimeout(() => {
+      /**
+       * 这个定时器函数，可以证明，
+       * 当change detection strategy变成OnPush时，
+       * 只有Input的reference改变才会触发change detection
+       */
+      // this.name.second = "mao";
+
       /**
        * Only with run change detection explicitly, 
        * view will update when you just change the name value.
        */
-      // this.name.second = "mao";
       // this.cdr.detectChanges();
       // this.cdr.markForCheck();
 
@@ -38,16 +42,19 @@ export class ChangeDetectComponent implements OnInit {
         first: "guo",
         second: "mao"
       };
+      this.age = "28";
     }, 5000);
   }
 
-  /**
-   * 这个User click事件可以证明，
-   * 当component和它的子component发生了user事件时，
-   * 同样也可以触发change detection
-   */
+
   onClick() {
-    this.name.second = "xuan";
+    /**
+     * 这个User click事件可以证明，
+     * 当component和它的子component发生了user事件时，
+     * 同样也可以触发change detection
+     */
+    this.name.first = "Luka";
+    this.name.second = "dan";
   }
 
 
--- src/app/change-detect/child.component.ts
+++ src/app/change-detect/child.component.ts
@@ -1,35 +1,47 @@
-import { Component, Input, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy, ChangeDetectorRef, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-change-detect-child',
-    template: `<h4>Reference Data:{{name.first}} {{name.second}}</h4>
-               <h4>Child:{{ runChangeDetection }}</h4> 
-               <h4>Async pipe{{ promiseData | async}}</h4>
+    template: `<h5>Reference Data:{{name.first}} {{name.second}}</h5>
+               <h5>Not reference data: {{age}}</h5>
+               <h5>Child getter:{{ runChangeDetection }}</h5>
+               <h5>Async pipe Promise {{ promiseData | async }}</h5>
                <button (click)="onClick()">User event trigger change detection</button>`,
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
-export class HelloComponent implements OnInit{
-    @Input() name;
-    promiseData: Promise<string> ;
+export class HelloComponent implements OnInit {
+    _age;
 
-    get runChangeDetection(): boolean{
+    @Input() name;
+    @Input() set age(value) {
+        this._age = value;
+        this.cdr.markForCheck();
+    }
+
+    promiseData: Promise<{}>;
+
+    get runChangeDetection(): boolean {
         console.log('Checking the child view');
         return true;
     }
 
+    constructor(private cdr: ChangeDetectorRef) { }
+
     ngOnInit(): void {
-       this.promiseData = this.getPromise();
+        this.promiseData = this.getPromise();
     }
+
+    onClick() {
     /**
      * 如果父亲component也设置了OnPush策略，
      * 子component的user事件也可以触发父亲component的change detection
      */
-    onClick() {
-        this.name.second = "maomao";
+        this.name.first = "Mao";
+        this.name.second = "mao";
     }
 
-    getPromise () {
+    getPromise() {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 console.log("start");
